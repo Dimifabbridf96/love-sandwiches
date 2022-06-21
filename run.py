8cad13b9cd7c6779c3ad569288d1821f852b0993
Dimifabbridf96 <dimi.fabbri@gmail.com>
2022-06-21 18:41:03 +0000

    Add user request loop reèeat request until valide values are provided

--- run.py
+++ run.py
@@ -16,15 +16,19 @@ def get_sales_data():
     """
     Get sales figures input from the user 
     """
-    print('Please enter the sales data from the last market.')
-    print('Data should be six numbers, separated by commas.')
-    print('Example: 10,20,30,40,50,60\n')
-    data_str = input('Enter your data here: ')
+    while True:
+        print('Please enter the sales data from the last market.')
+        print('Data should be six numbers, separated by commas.')
+        print('Example: 10,20,30,40,50,60\n')
+        data_str = input('Enter your data here: ')
     
-    sales_data = data_str.split(',')
+        sales_data = data_str.split(',')
+        
+        if validate_data(sales_data):
+            print('Data is valid!')
+            break
 
-    validate_data(sales_data)
-
+    return sales_data
 def validate_data(values):
     """
     Inside the try, converts all string values into integers.
@@ -40,7 +44,8 @@ def validate_data(values):
             ) 
     except ValueError as e:
         print(f"Invalide data: {e}, please try again.\n")
+        return False
+    return True
 
 
-
-get_sales_data()
+data = get_sales_data()
