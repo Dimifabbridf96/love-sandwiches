c7ecc508668c9846974ec744bbaca5001b5bd8ae
Dimifabbridf96 <dimi.fabbri@gmail.com>
2022-06-22 12:51:43 +0000

    add updating  worksheets

--- run.py
+++ run.py
@@ -66,6 +66,12 @@ def update_surplus_worksheet(surplus_data):
     surplus_worksheet.append_row(surplus_data)
     print("Sales sheet updated successfully.\n")
 
+def update_worksheet(data, worksheet):
+    print(f"Updating {worksheet} worksheet...\n")
+    worksheet_to_update = SHEET.worksheet(worksheet)
+    worksheet_to_update.append_row(data)
+    print(f"{worksheet} updated succesfully.\n")
+
 def calculate_surplus_data(sales_row):
     """
     compare sales with stock values to get the surplus value.
@@ -76,7 +82,6 @@ def calculate_surplus_data(sales_row):
     print("Calculating surplus data...\n")
     stock = SHEET.worksheet('stock').get_all_values()
     stock_row = stock.pop()
-    print(f"stock_row : {stock_row} sales_row: {sales_row}")
 
     surplus_data = []
     for sales, stock in zip(sales_row, stock_row):
@@ -91,9 +96,8 @@ def main():
     """
     data = get_sales_data()
     sales_data = [int(num) for num in data]
-    update_sales_worksheet(sales_data)
+    update_worksheet(sales_data, 'sales')
     new_surplus_data = calculate_surplus_data(sales_data)
-    update_surplus_worksheet(new_surplus_data)
-    
+    update_worksheet(new_surplus_data, 'surplus')
 print("Welcome to Love Sandwiches data Automation")
 main()
